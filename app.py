bf94b7a4a9463a158aaccea959bdef6b25247703
Lavichs <barmalej54d132@gmail.com>
2025-06-19 01:04:14 +0500

    feat: add endpoint to get note

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import uuid
 
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse
 import markdown
 from pydantic import BaseModel
 import aiofiles
@@ -25,3 +26,16 @@ async def add(
     except Exception as e:
         print(e)
         return HTTPException(status_code=500)
+
+@app.get("/notes/{uid}")
+async def get_note(uid: str):
+    try:
+        async with aiofiles.open(f'notes/{uid}.md', mode='r') as f:
+            content = await f.read()
+            return HTMLResponse(content=content, status_code=200)
+    except FileNotFoundError as e:
+        return HTTPException(status_code=404, detail=f'Note {uid} not found')
+    except Exception as e:
+        print(e)
+        return HTTPException(status_code=500)
+
